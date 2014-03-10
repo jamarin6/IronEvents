e7edcf780694b204599cd98053d0dd84b1d21b68
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-10 13:56:02 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IronEvents::Application.config.secret_key_base = '52c95853c5fd5404d6941928b3ffd000dc5e4bdd5460d4269fd9669d7b52c90bc05b1cf0881327418314e588f509483cda02d58938cbdef8fd0bbf2daadba381'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IronEvents::Application.config.session_store :cookie_store, key: '_IronEvents_session'
